98187422b602fa271c640615b4d9a4970c6864d4
jedenzero <134185805+jedenzero@users.noreply.github.com>
2024-01-21 12:55:30 +0900

    Update neatian-list.js

---
+++ neatian/neatian-list.js
@@ -0,0 +1,8 @@
+var list=[
+    {type:"ls",title:"0. 네어트어란"},
+    {type:"wr", detail:"작문", title:"단문 1 - 유성"},
+    {type:"wr", detail:"작문", title:"단문 2 - 크리스마스"},
+    {type:"wr", detail:"번역", title:"단문 3 - 달"},
+    {type:"wr", detail:"번역", title:"고독 - Ella Wheeler Wilcox"},
+    {type:"wr", detail:"개발 과정", title:"접미사가 붙은 동사"}
+]
