84866a4df4f7b6207d091e151219a583887e78b2
jedenzero <134185805+jedenzero@users.noreply.github.com>
2024-01-25 22:01:58 +0900

    Update neatian-list.js

--- neatian/neatian-list.js
+++ neatian/neatian-list.js
@@ -4,5 +4,6 @@ var list=[
     {type:"wr", detail:"작문", title:"단문 2 - 크리스마스"},
     {type:"wr", detail:"번역", title:"단문 3 - 달"},
     {type:"wr", detail:"개발 과정", title:"접미사가 붙은 동사"},
-    {type:"wr", detail:"개발 과정", title:"초기 네어트어 구상"}
+    {type:"wr", detail:"개발 과정", title:"초기 네어트어 구상"},
+    {type:"ls", title:"1. 동사와 부정"}
 ]
