8bcc13de78f172ae6bdc677968528d2859998696
jedenzero <134185805+jedenzero@users.noreply.github.com>
2024-02-04 13:04:48 +0900

    Update neatian-list.js

--- neatian/neatian-list.js
+++ neatian/neatian-list.js
@@ -6,5 +6,6 @@ var list=[
     {type:"wr", detail:"개발 과정", title:"접미사가 붙은 동사"},
     {type:"wr", detail:"개발 과정", title:"초기 네어트어 구상"},
     {type:"ls", title:"1. 동사와 부정"},
-    {type:"ls", title:"2. 조사"}
+    {type:"ls", title:"2. 조사"},
+    {type:"ls", title:"3. 형용사화"}
 ]
