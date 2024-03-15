55a82502f72e56d0cc7024245758ee20cabb7a3e
jedenzero <134185805+jedenzero@users.noreply.github.com>
2024-03-15 21:37:26 +0900

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -143,7 +143,7 @@ var div=document.createElement('div');
 var divI=''; //div 임시
 div.className='word';
 divI+='<sup class="id">'+row[4]+'</sup>'; //id 추가
-divI+='<h2 style="display:inline-block;margin-bottom:0;" onclick=\"individualMore('+changeToString(row)+');\">'+row[0]+'</h2>' //단어 추가
+divI+='<h2 style="display:inline-block;margin-bottom:0;" onclick=\"individualMore(\''+row.stringify().replace(/'/g, "\\'")+'\');">'+row[0]+'</h2>' //단어 추가
 //어원 추가
 if(row[5]){
 var i=0;
@@ -250,16 +250,5 @@ divI+='</table>';
 div.innerHTML=divI;
 modal.appendChild(div);
 modal.style.visibility='visible';
-}
-function changeToString(arr) {
-    return '[' + arr.map(item => {
-        // 배열의 요소가 문자열일 때 이스케이프 처리 및 따옴표 추가
-        if (typeof item==='string') {
-            return '\"' + item.replace(/"/g, '\\"') + '\"';
-        }
-	else{
-            return item;
-        }
-    }).join() + ']';
 }
 resetLang();
