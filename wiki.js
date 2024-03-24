9551228da135d6c66b3b19a581549c436ac6ce99
1010 <134185805+jedenzero@users.noreply.github.com>
2024-03-24 18:31:48 +0900

    Update and rename multidictionary.js to wiki.js
    임시 주석처리

--- wiki.js
+++ wiki.js
@@ -1,4 +1,11 @@
-//임시
+/**
+fetch("https://drive.google.com/drive/folders/"+drive)
+.then(response=>{
+	return response.text();
+})
+.then(data=>{
+	console.log(data.split('modelChunk = ')[1].split('"s":\"')[1].split('\"')[0]);
+});
 fetch("https://docs.google.com/document/d/1hZ2Uanz7YHneXI4MxaXWYTAVrF4EuO1HMEUZlC79ZE8/edit")
 .then(response=>{
 	return response.text();
@@ -6,3 +13,4 @@ fetch("https://docs.google.com/document/d/1hZ2Uanz7YHneXI4MxaXWYTAVrF4EuO1HMEUZl
 .then(data=>{
 	console.log(data.split('modelChunk = ')[1].split('"s":\"')[1].split('\"')[0]);
 });
+**/
