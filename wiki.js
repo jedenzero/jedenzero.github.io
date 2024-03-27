0fdaadc0bddfb774508e6c68d091071b67dfa8b2
1010 <134185805+jedenzero@users.noreply.github.com>
2024-03-27 20:26:54 +0900

    Update wiki.js
    구글 독스에는 <웹에 게시>라는 기능이 있다!

--- wiki.js
+++ wiki.js
@@ -1,27 +1,36 @@
 var docs=[];
 var doc=front;
-if(new URL(window.location.href).searchParams.get('id')){
-doc=new URL(window.location.href).searchParams.get('id');
+var imsi=document.createElement('div');
+if(new URL(window.location.href).searchParams.get('doc')){
+doc=new URL(window.location.href).searchParams.get('doc');
 }
 //문서 목록 불러오기
 fetch('https://sheets.googleapis.com/v4/spreadsheets/'+sheet+'/values/docs!A:C?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk')
 .then(response=>response.json())
 .then(data=>{
 docs=data.values;
-document.getElementById('contain').innerHTML='<h2 style="text-align:center;">'+docs.find(row=>row[1]===doc)[0];
 //문서 불러오기
-fetch("https://docs.google.com/document/d/"+doc+"/edit")
+fetch("https://docs.google.com/document/d/e/"+docs.find(row=>row[0]===doc)[1]+"/pub")
 .then(response=>{
 	return response.text();
 })
 .then(data=>{
-	document.getElementById('contain').innerHTML+=data.split('modelChunk = ')[1].split('"s":\"')[1].split('\"')[0].replace(/\\u([\d\w]{4})/gi, function (match, grp) {
-    		return String.fromCharCode(parseInt(grp, 16));
-  	});
-	if(!new URL(window.location.href).searchParams.get('id')||new URL(window.location.href).searchParams.get('id')===front){
+	var i='';
+	for(let el of data.split(/<p|<\/p>/).slice(2)){
+		if(el.startsWith('</div>')){
+			break;
+		}
+		if(el===''){
+			continue;
+		}
+		i+='&lt;p&gt;'+el.substring(el.indexOf('>')+1)+'&lt;/p&gt;';
+	};
+	imsi.innerHTML=i;
+    	document.getElementById('contain').innerHTML='<h2 style="text-align:center;">'+doc+'</h2>'+imsi.textContent;
+	if(!new URL(window.location.href).searchParams.get('doc')||new URL(window.location.href).searchParams.get('doc')===front){
 	document.getElementById('contain').innerHTML+='<h3>문서 목록</h3>';
 	docs.forEach(row=>{
-		document.getElementById('contain').innerHTML+='<p><a href=\"?id='+row[1]+'\">'+row[0]+'</a></p>';
+		document.getElementById('contain').innerHTML+='<p><a href=\"?doc='+row[0]+'\">'+row[0]+'</a></p>';
 	});
 	}
 });
